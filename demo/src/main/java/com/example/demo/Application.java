40c7fedc2ce3be1ada6705bad3ae9d40bed32c9a
luweifeng <luweifeng@dianwoda.com>
2018-12-12 11:47:17 +0800

    支持springboot

--- demo/src/main/java/com/example/demo/Application.java
+++ demo/src/main/java/com/example/demo/Application.java
@@ -1,11 +1,12 @@
 package com.example.demo;
 
+import com.qa.framework.FrameworkSpringConfiguration;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Import;
 
 @SpringBootApplication
-@ComponentScan({"com.qa.framework", "com.example.demo"})
+@Import({FrameworkSpringConfiguration.class})
 public class Application {
 
     public static void main(String[] args) {
